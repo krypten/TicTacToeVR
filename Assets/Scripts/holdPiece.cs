0e680f0c662f11adc28435feca83f3f3bb811066
Chaitanya Agrawal <agrawac@amazon.com>
2018-06-24 17:44:45 +0530

    Added optimizations in scripts and fix drag issue
    Added optimizations in scripts by caching GetComponent calls. Also fixed drag and mass of O pieces in tic tac toe.

--- Assets/Scripts/holdPiece.cs
+++ Assets/Scripts/holdPiece.cs
@@ -16,14 +16,25 @@ public class holdPiece : MonoBehaviour {
 	public float gravityFactor = 10.0f;
 	private Vector3 forceDirection;
 
+	private BoxCollider boxColliderComponent;
+	private GameLogic gameLogicComponent;
+	private Rigidbody rigidBodyComponent;
+	private PlayerPiece playerPieceComponent;
+
     // Use this for initialization
     void Start () {
-		
 	}
+
 	public void grabPiece(GameObject selectedPiece) {
         if (selectedPiece.GetComponent<PlayerPiece>().hasBeenPlayed == false) {
             pieceBeingHeld = selectedPiece;
-            holdingPiece = true;
+			
+			boxColliderComponent = pieceBeingHeld.GetComponent<BoxCollider> ();
+			gameLogicComponent = GameLogic.GetComponent<GameLogic>();
+			playerPieceComponent = pieceBeingHeld.GetComponent<PlayerPiece>();
+			rigidBodyComponent = pieceBeingHeld.GetComponent<Rigidbody> ();
+			
+			holdingPiece = true;
         }
     }
 	// Update is called once per frame
@@ -33,40 +44,26 @@ public class holdPiece : MonoBehaviour {
                 Vector3 forwardDir = raycastHolder.transform.TransformDirection(Vector3.forward) * 100;
                 Debug.DrawRay(raycastHolder.transform.position, forwardDir, Color.green);
 
-
                 if (Physics.Raycast(raycastHolder.transform.position, (forwardDir), out hit)) {
 					gravityAttractor.transform.position = new Vector3(hit.point.x, hit.point.y + hoverHeight, hit.point.z);
 
+					rigidBodyComponent.useGravity = false;
+					boxColliderComponent.enabled = false;
 
-					pieceBeingHeld.GetComponent<Rigidbody> ().useGravity = false;
-					pieceBeingHeld.GetComponent<BoxCollider> ().enabled = false;
-
-					pieceBeingHeld.GetComponent<Rigidbody>().AddForce(gravityAttractor.transform.position - pieceBeingHeld.transform.position);
-
+					rigidBodyComponent.AddForce(gravityAttractor.transform.position - pieceBeingHeld.transform.position);
 
                     if (hit.collider.gameObject.tag == "Grid Plate") {
                         if (Input.GetMouseButtonDown(0)) {
                             holdingPiece = false;
                             hit.collider.gameObject.SetActive(false);
-                            pieceBeingHeld.GetComponent<PlayerPiece>().hasBeenPlayed = true;
-							pieceBeingHeld.GetComponent<Rigidbody> ().useGravity = true;
-							pieceBeingHeld.GetComponent<BoxCollider> ().enabled = true;
-                            GameLogic.GetComponent<GameLogic>().playerMove(hit.collider.gameObject);
+                            playerPieceComponent.hasBeenPlayed = true;
+                            rigidBodyComponent.useGravity = true;
+                            boxColliderComponent.enabled = true;
+                            gameLogicComponent.playerMove(hit.collider.gameObject);
                         }
-
                     }
-
                 }
             }
         }
     }
-
 }
-
-
-
-
-
-
-
-
